8a31d80c92fc7cdfaeeafb70362855e46d5b3395
rgrave <rgrave@ethz.ch>
2023-04-19 17:00:09 +0200

    Get image stats uses all available cores

--- data/get_image_stats.py
+++ data/get_image_stats.py
@@ -8,20 +8,24 @@ import os
 import pandas as pd
 import numpy as np
 from PIL import Image
+import multiprocessing
+import itertools
+from functools import partial
 
+def split_array_in_number_of_cores(data_list:list, cores_amount:int):
+    l = len(data_list)
+    step = l //cores_amount 
+    for i in range(0,l,step):
+        yield data_list[i:i+step]
+        
+        
 def slices_generator(data_list:list, step:int):
-    num = 0
     l = len(data_list)
     for i in range(0,l,step):
-        # if i >=l:
-        #     break
         yield data_list[i:i+step]
-        num += 1
 
 
 def get_partial_sums(filenames:list,total_len:int):
-    # print("Did:",len(filenames))
-    dataset_array_list = []
     partial_full_sum, partial_full_squared_sum = np.zeros((3,),dtype=np.float64),np.zeros((3,),dtype=np.float64)
     for image_name in filenames:
          
@@ -29,26 +33,31 @@ def get_partial_sums(filenames:list,total_len:int):
         partial_full_sum += image.mean(axis=(0,1))/total_len
         partial_full_squared_sum  += (np.square(image)).mean(axis=(0,1))/total_len
 
-        # dataset_array_list.append(image)
-        # print(np.max(image))
-    
+
 
     return partial_full_sum, partial_full_squared_sum;
 
 
-def get_dataset_stats(csv_filename,slice_step=20):
+def get_dataset_stats(csv_filename):
+    
     files = pd.read_csv(csv_filename)["ToneMapped"]
     full_sum, full_squared_sum = np.zeros((3,)),np.zeros((3,))
     files_len = len(files)
-    for slice in slices_generator(files,slice_step):
-        sum_over_partial_dataset, squared_sum_over_partial_dataset = get_partial_sums(slice,files_len)
+    cpu_counts = multiprocessing.cpu_count()
+    with multiprocessing.Pool(cpu_counts) as pool:
+        
+        res = pool.map(partial(get_partial_sums, total_len=files_len), split_array_in_number_of_cores(files,cpu_counts))
+    for [sum_over_partial_dataset,squared_sum_over_partial_dataset] in res:
         full_sum += sum_over_partial_dataset
         full_squared_sum += squared_sum_over_partial_dataset
-
+    # res = np.array(res)
+    # mean_over_entire_dataset = res[:,0,:].mean(axis=0)
+    # std_over_entire_dataset = np.sqrt(res[:,1,:].mean(axis=0) - np.square(mean_over_entire_dataset) )
     
-    mean_over_entire_dataset = full_sum 
-    std_over_entire_dataset = np.sqrt(full_squared_sum - np.square(mean_over_entire_dataset) )
-    return mean_over_entire_dataset/255, std_over_entire_dataset/255;
+    mean_over_entire_dataset1 = full_sum 
+    std_over_entire_dataset1 = np.sqrt(full_squared_sum - np.square(mean_over_entire_dataset1) )   
+    
+    return mean_over_entire_dataset1/255, std_over_entire_dataset1/255;
 
 
 
@@ -65,8 +74,8 @@ def get_dataset_stats2(csv_filename):
     return mean_over_entire_dataset / 255, np.sqrt(std_over_entire_dataset - np.square(mean_over_entire_dataset))/ 255;
 
 def main():
-    mean, std = get_dataset_stats("downloads/image_files.csv")
-    mean2, std2 = get_dataset_stats2("downloads/image_files.csv")
+    mean, std = get_dataset_stats("./downloads/images_files.csv")
+    mean2, std2 = get_dataset_stats2("./downloads/images_files.csv")
     print(mean)
     print(mean2)
     print(std)
