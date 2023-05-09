ce242b9dff55f87bfc9ab3d7daaa8980e1b7f3a5
rgrave <rgrave@ethz.ch>
2023-05-09 14:37:40 +0200

    missed one file

--- data/convert_distance_to_depth.py
+++ data/convert_distance_to_depth.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-
+from PIL import Image
+import os
 #Arg: pass depth image as numpy array
 #return: returns depth values as numpy array
 def convert_distance_to_depth(np_depth_array):
@@ -17,16 +18,21 @@ def convert_distance_to_depth(np_depth_array):
 class ConvertSemanticLabelsToRGB(object):
 
     def __init__(self):
-        self.mappings = pd.read_csv("semantic_label_descs.csv",usecols=["semantic_color_r", "semantic_color_g", "semantic_color_b"])
+        path = os.path.abspath(os.path.dirname(__file__))
+        self.mappings = pd.read_csv(path+"/semantic_label_descs.csv",usecols=["semantic_color_r", "semantic_color_g", "semantic_color_b"])
 
     def __call__(self, image):
         return self.convert_semantic_label_to_rgb(image)
 
     def convert_semantic_label_to_rgb(self, image):
-        rgb_image = np.empty(shape=(image.shape[0], image.shape[1], 3), dtype=np.int64)
+        rgb_image = np.empty(shape=(image.size[0], image.size[1], 3), dtype=np.uint8)
+        print("SIZE:",image.size)
         mask = (image == -1)
         rgb_image[mask, :] = [255, 255, 255]  # white
+        print(f"rgbimage shape bef: {rgb_image.shape}")
         for label in range(1, 40):
             rgb_image[image == label] = self.mappings.iloc[label - 1]
+        print(f"rgbimage shape af: {rgb_image.shape}")
+        print(f"min:{rgb_image.min()} , max:{rgb_image.max()}")
 
-        return rgb_image
+        return Image.fromarray(rgb_image) 
