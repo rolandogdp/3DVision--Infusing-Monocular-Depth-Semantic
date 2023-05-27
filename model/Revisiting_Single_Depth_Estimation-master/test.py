45952064268ed9faae0d6a2455886a0687c5aa22
rolandogdp <rgrave@student.ethz.ch>
2023-05-27 14:37:34 +0200

    Modified test.py to add support for the Method.JOINTLEARNING

--- model/Revisiting_Single_Depth_Estimation-master/test.py
+++ model/Revisiting_Single_Depth_Estimation-master/test.py
@@ -70,7 +70,7 @@ def test(test_loader, model, thre):
     for i, sample_batched in enumerate(test_loader):
         image, depth = sample_batched['image'], sample_batched['depth']
 
-        if(torch.cuda.is_available()):
+        if(torch.cuda.is_availabl()):
             depth = depth.cuda(non_blocking=True) #
             image = image.cuda()
 
@@ -137,7 +137,97 @@ def test(test_loader, model, thre):
         w.writerow(averageError)
 
 
-   
+
+def test_sample_joint(test_loader, model, thre): 
+    model.eval()
+    totalNumber = len(test_loader)
+
+    Ae = 0
+    Pe = 0
+    Re = 0
+    Fe = 0
+
+    errorSum = {'MSE': 0, 'RMSE': 0, 'ABS_REL': 0, 'LG10': 0,
+                'MAE': 0,  'DELTA1': 0, 'DELTA2': 0, 'DELTA3': 0}
+    
+    depth_results = []
+    sgmentation_results = []
+    if(torch.cuda.is_available()):
+        model.to("cuda:0")
+    else:model.to("cpu")
+
+    for i, sample_batched in enumerate(test_loader):
+        torch.cuda.empty_cache()
+        
+        image, depth = sample_batched['image'], sample_batched['depth']
+        
+        if(torch.cuda.is_available()):
+            depth = depth.cuda(non_blocking=True) #
+            image = image.cuda()
+        image = torch.autograd.Variable(image, requires_grad=False)
+        depth = torch.autograd.Variable(depth, requires_grad=False)
+        depth_output, segmentation_output = model(image)
+        depth_output = torch.nn.functional.interpolate(depth_output, size=[depth.size(2),depth.size(3)], mode='bilinear')
+
+        _output, _depth, num_non_nans = util.setNanToZero(depth_output, depth)
+        
+
+
+        depth_edge = edge_detection(_depth,1)
+        output_edge = edge_detection(_output,1)
+
+        batchSize = depth.size(0)
+        errors = util.evaluateError(_output, _depth, num_non_nans)
+        errorSum = util.addErrors(errorSum, errors, batchSize)
+        edge1_valid = (depth_edge > thre)
+        edge2_valid = (output_edge > thre)
+
+        nvalid = np.sum(torch.eq(edge1_valid, edge2_valid).float().data.cpu().numpy())
+        A = nvalid / num_non_nans#(depth.size(2)*depth.size(3)) #how many pixel are the same in edge map in percentage
+
+        nvalid2 = np.sum(((edge1_valid + edge2_valid) ==2).float().data.cpu().numpy()) #number of true positive
+        P = nvalid2/(np.sum(edge2_valid.data.cpu().numpy())) #precision
+        R = nvalid2/(np.sum(edge1_valid.data.cpu().numpy())) #recall
+
+        epsilon = 10**(-10)
+        F = (2 * P * R) / (P + R + epsilon) #precision and recall?
+
+        Ae += A
+        Pe += P
+        Re += R
+        Fe += F
+
+    Av = (Ae / totalNumber).item()
+    Pv = Pe / totalNumber
+    Rv = Re / totalNumber
+    Fv = Fe / totalNumber
+    print(Av)
+
+    segmentationError = {'Precision_of_EdgeMap': Pv, 'Recall_of_EdgeMap': Rv, 'F_Measure': Fv, 'Relative_EdgeMap_Error': Av}
+    averageError = util.averageErrors(errorSum, totalNumber)
+    averageError['RMSE'] = np.sqrt(averageError['MSE'])
+
+    print(averageError)
+    print(segmentationError)
+
+    now = datetime.datetime.now()
+    filename_date = f".{str(now.strftime('%m-%d-%Y-%H-%M-%S'))}"
+    filename_error_norms = f"test_-{filename_date}-{my_method}_error_norms.csv"
+    path = f"{os.environ['THREED_VISION_ABSOLUTE_DOWNLOAD_PATH'] + '../outputs/results/test_error_norms/'}"
+    keys = {'MSE', 'RMSE', 'ABS_REL', 'LG10',
+                    'MAE', 'DELTA1', 'DELTA2', 'DELTA3', 'Precision_of_EdgeMap', 'Recall_of_EdgeMap', 'F_Measure', 'Relative_EdgeMap_Error'}
+
+    averageError.update(segmentationError)
+    print(type(averageError))
+    print(averageError)
+
+    with open(path + filename_error_norms, mode="w", newline='') as file:
+        w = csv.DictWriter(file, averageError.keys())
+        w.writeheader()
+        w.writerow(averageError)
+
+        
+    return depth_results,sgmentation_results  
 
 def edge_detection(depth,channel_inputs=1):
     get_edge = sobel.Sobel(channel_inputs).to(device) #.cuda()
